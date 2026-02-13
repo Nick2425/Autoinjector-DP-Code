21b6246045870aa67730cdc0dc6f8b90ed3487b8
Nick <170993098+Nick2425@users.noreply.github.com>
2026-02-12 21:44:37 -0500

    Finalized force sensor  graphs

--- rolling_average/rolling_average_simulation.py
+++ rolling_average/rolling_average_simulation.py
@@ -6,15 +6,15 @@ import numpy as np
 import math
 import simulate_force_sensors as rvs
 
-LOOP_DELAY = 0.1
+LOOP_DELAY = 0.05
 FORCE_THRESHOLD = 0
 HOLD_TIME = 3
 
 def test_rolling_average():
 
     data_list = [[],[],[]] #### EMPTY DATA SETS
-    FSR_list = [rvs.ra_simulators(2), rvs.ra_simulators(2), rvs.ra_simulators(2)] #### SIMULATED FORCE SENSOR LIST
-    RA_list: object = [] ### PREDEFINED ROLLING AVERAGE LIST
+    FSR_list = [rvs.ra_simulators(0.555), rvs.ra_simulators(0.555), rvs.ra_simulators(0.555)] #### SIMULATED FORCE SENSOR LIST
+    RA_list = [] ### PREDEFINED ROLLING AVERAGE LIST
     time_list = []
 
     above_threshold = False
@@ -37,13 +37,15 @@ def test_rolling_average():
             data_list[i] = update_list(data_list[i], FSR_list[i])
         RA_list.clear() ### RESETS ROLLING AVERAGE LIST
         for i in range(3):
-            RA_list.append(FSR_rolling_average(data_list[i])) ##RE APPENDS ROLLING AVERAGE LIST
-        print(data_list[1])
+            average = FSR_rolling_average(data_list[i])
+            RA_list.append(average) ##RE APPENDS ROLLING AVERAGE LIST
+
+        time_list = generate_time_list(len(data_list[0]))
         #### Compares rolling averages
         if RA_list[0] != None and RA_list[1] != None and RA_list[2] != None:
             if RA_list[0] > FORCE_THRESHOLD and RA_list[1] > FORCE_THRESHOLD and RA_list[2] > FORCE_THRESHOLD: 
                 ### imcrements time the sensor is pushed for
-                time_pressed += 3*LOOP_DELAY #### adjusts for unknown time delay
+                time_pressed += LOOP_DELAY #### adjusts for unknown time delay
                 # checks if sensor is held for hold time
                 if time_pressed > HOLD_TIME:
                     above_threshold = True
@@ -63,9 +65,11 @@ def test_rolling_average():
         for i in range(3):
             lines[i].set_xdata(time_list)
             lines[i].set_ydata(data_list[i])
-        
+            ax[i].set_xlabel(f"Relative Time (s) | Rolling Average {i} = {RA_list[i]}", fontweight="bold")
+            ax[i].relim()
         # Rescale axes automatically if needed
         
+    
         fig.canvas.draw()
         fig.canvas.flush_events()
         plt.pause(LOOP_DELAY) # Pause to allow the GUI event loop to run
@@ -75,32 +79,27 @@ def test_rolling_average():
     plt.show()
 
 def generate_plot():
-    fig, ax = plt.subplots(3)
+    fig, ax = plt.subplots(3, sharex=True, sharey=True)
     for i in range(3):
-        ax[i].set_xlim(-5, 0)
-        ax[i].set_ylim(-5,260)
+        ax[i].set_xlim(-3, 0)
+        ax[i].set_ylim(-3,160)
         ax[i].set_xlabel("Relative Time (s)", fontweight="bold")
         ax[i].set_ylabel("Force (N)", fontweight="bold")
 
-        ax[i].set_xticks([-5, -4, -3, -2, -1, 0])
-        ax[i].set_yticks([0, 50, 100, 150, 200, 250])
+        ax[i].set_xticks([-3, -2, -1, 0])
+        ax[i].set_yticks([0, 50, 100, 150])
 
     return fig, ax
-
-
-###### CREATES SIMULATED FORCE VALUES BASED ON A TRAPEZOIDIAL SINE WAVE
-
 
 ##### APPENDS FORCE VALUES TO THE LIST
 def update_list(inputed_list, sensor):
     updated_list = inputed_list.copy()
-    if len(updated_list) < 30:
+    if len(updated_list) < 150:
         updated_list.append(sensor.read_force())
     else:
         updated_list.pop(0)
         updated_list.append(sensor.read_force())
     return updated_list
-
 
 ### GENERATES A LIST OF TIMES DISPLAYED ON THE PLOT
 def generate_time_list(n: int):
@@ -111,16 +110,12 @@ def generate_time_list(n: int):
         element += LOOP_DELAY
     return time
 
-
 ####### ROLLING AVERAGE CODE COPIED
 def FSR_rolling_average(datalist):
-  if len(datalist) < 50:
+  if len(datalist) < 30:
     return None
   else:
-    rolling_av = sum(datalist)/50
-    return rolling_av
-
-
-
+    rolling_av = sum(datalist, -30)/30
+    return round(rolling_av, 2)
 
 test_rolling_average()
--- rolling_average/simulate_force_sensors.py
+++ rolling_average/simulate_force_sensors.py
@@ -2,15 +2,18 @@ import time
 import math
 import random
 
+
+###### CREATES SIMULATED FORCE VALUES BASED ON A TRAPEZOIDIAL SINE WAVE
+
+
 time_passed = 0
 class ra_simulators():
-    def __init__(self, variability = 0) -> None:
+    def __init__(self, variability = 0.000) -> None:
         pass
         self.reading = 0
         self.variability = variability
 
     def read_force(self):
         global time_passed
-        value = max(0,min(1,2*(math.sin((time_passed - 2 - random.uniform(-1*self.variability, self.variability))))))
+        value = max(0,min(1,2*(math.sin((time_passed - 2 - random.uniform(-0.555*self.variability, 0.555*self.variability))))))
         return 140*value
-        
