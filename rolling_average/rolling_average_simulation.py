64ff6d30818e0f2edd06df00dcf2640fba358260
Nick <170993098+Nick2425@users.noreply.github.com>
2026-02-12 22:04:03 -0500

    Finalized comments for simulation

--- rolling_average/rolling_average_simulation.py
+++ rolling_average/rolling_average_simulation.py
@@ -10,17 +10,21 @@ LOOP_DELAY = 0.05
 FORCE_THRESHOLD = 0
 HOLD_TIME = 3
 
+
+##### TEST ROLLING AVERAGE
 def test_rolling_average():
 
+    ### Here the force data lists are predefined
     data_list = [[],[],[]] #### EMPTY DATA SETS
     FSR_list = [rvs.ra_simulators(0.555), rvs.ra_simulators(0.555), rvs.ra_simulators(0.555)] #### SIMULATED FORCE SENSOR LIST
     RA_list = [] ### PREDEFINED ROLLING AVERAGE LIST
     time_list = []
 
+    #### initial variables
     above_threshold = False
     time_pressed = 0
 
-    ### MATPLOB LIB
+    ### MATPLOB LIB CONFIG
     plt.ion()
     fig, ax = generate_plot()
     fig.suptitle("Force Sensor Graphs")
@@ -30,31 +34,33 @@ def test_rolling_average():
     for i in range(3):
         line = ax[i].plot([], [])[0]
         lines.append(line)
-    ###### THIS IS THE REGULAR SENSING STUFF NOW
+
+    ###### Loop in which sensing occurs
     
     while above_threshold != True:
-        for i in range(3):
-            data_list[i] = update_list(data_list[i], FSR_list[i])
+
+        #### Updates existing rolling averages.
         RA_list.clear() ### RESETS ROLLING AVERAGE LIST
         for i in range(3):
+            data_list[i] = update_list(data_list[i], FSR_list[i])
             average = FSR_rolling_average(data_list[i])
-            RA_list.append(average) ##RE APPENDS ROLLING AVERAGE LIST
-
+            RA_list.append(average) ##REAPPENDS ROLLING AVERAGE LIST
         time_list = generate_time_list(len(data_list[0]))
-        #### Compares rolling averages
+
+
+        #### Compares rolling averages and checks if they are defined or are null
         if RA_list[0] != None and RA_list[1] != None and RA_list[2] != None:
-            if RA_list[0] > FORCE_THRESHOLD and RA_list[1] > FORCE_THRESHOLD and RA_list[2] > FORCE_THRESHOLD: 
-                ### imcrements time the sensor is pushed for
-                time_pressed += LOOP_DELAY #### adjusts for unknown time delay
-                # checks if sensor is held for hold time
-                if time_pressed > HOLD_TIME:
-                    above_threshold = True
-            ### checks if person lets go and resets if necessary.
-            else:
-                if time_pressed > 0:
-                    time_pressed = 0
+            if RA_list[0] > FORCE_THRESHOLD and RA_list[1] > FORCE_THRESHOLD and RA_list[2] > FORCE_THRESHOLD:  ### imcrements time the sensor is pushed for
+                time_pressed += LOOP_DELAY                                                                      ### adjusts for unknown time delay
+                if time_pressed > HOLD_TIME:                                                                    ### checks if sensor is held for the total hold time
+                    above_threshold = True 
+                    break                                                                                       ### This exists the loop
 
 
+            
+            else:                           #### checks if person lets go and resets if necessary.
+                if time_pressed > 0:        #### Resets the hold time if the averages aren't all above 0
+                    time_pressed = 0        #### which can imply that the device is in contact at the wrong angle and therefore can malfunction
 
         time.sleep(LOOP_DELAY)
 
@@ -66,17 +72,18 @@ def test_rolling_average():
             lines[i].set_xdata(time_list)
             lines[i].set_ydata(data_list[i])
             ax[i].set_xlabel(f"Relative Time (s) | Rolling Average {i} = {RA_list[i]}", fontweight="bold")
-            ax[i].relim()
-        # Rescale axes automatically if needed
-        
+            ax[i].relim()        
     
         fig.canvas.draw()
         fig.canvas.flush_events()
-        plt.pause(LOOP_DELAY) # Pause to allow the GUI event loop to run
+        plt.pause(LOOP_DELAY) 
         rvs.time_passed += LOOP_DELAY
 
     plt.ioff()
     plt.show()
+
+
+#### This function generates subplots for the graphs
 
 def generate_plot():
     fig, ax = plt.subplots(3, sharex=True, sharey=True)
@@ -92,6 +99,7 @@ def generate_plot():
     return fig, ax
 
 ##### APPENDS FORCE VALUES TO THE LIST
+
 def update_list(inputed_list, sensor):
     updated_list = inputed_list.copy()
     if len(updated_list) < 150:
@@ -118,4 +126,5 @@ def FSR_rolling_average(datalist):
     rolling_av = sum(datalist, -30)/30
     return round(rolling_av, 2)
 
+#### Begins simulation
 test_rolling_average()
