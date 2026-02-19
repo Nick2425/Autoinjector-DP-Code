530142463aa9ce6da7fbb3e657063ab75c29e722
yudawenya20-svg <yudawenya20@gmail.com>
2026-02-19 17:08:45 -0500

    Update injection.py

--- injection.py
+++ injection.py
@@ -1,5 +1,13 @@
 import math
 import sys
-Radius = #enter radius
-Conver_Constant = math.pi * Radius * 2
-
+from gpiozero import Servo
+#Radius = #enter radius
+#Conver_Constant = math.pi * Radius * 2
+servo = Servo (26)
+def test():
+  servo.value=-1
+  time.sleep(2)
+  servo.value=0
+  time.sleep(2)
+  servo.value=1
+test()
