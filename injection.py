cf59fdd74717f37bff5705809ef279bac1e71374
yudawenya20-svg <yudawenya20@gmail.com>
2026-02-20 14:04:17 -0500

    Update injection.py

--- injection.py
+++ injection.py
@@ -4,6 +4,7 @@ import time
 from gpiozero import Servo
 #Radius = #enter radius
 #Conver_Constant = math.pi * Radius * 2
+Conver_Constant = 6*math.pi
 def inject_amount(count, dosage):
   current_amount = (count+1) * dosage
   distance = ((current_amount / Conver_Constant)*(2/1.7))-1
