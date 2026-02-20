12ef00e258e2472390a66f8dc4ee9a25856bc866
yudawenya20-svg <yudawenya20@gmail.com>
2026-02-19 21:15:16 -0500

    Update injection.py

--- injection.py
+++ injection.py
@@ -1,13 +1,8 @@
 import math
 import sys
+import time
 from gpiozero import Servo
 #Radius = #enter radius
 #Conver_Constant = math.pi * Radius * 2
-my_servo = Servo(17,min_pulse_width=0.75/1000, max_pulse_width=2.25/1000)
-def test():
-  servo.value=-1
-  time.sleep(2)
-  servo.value=0
-  time.sleep(2)
-  servo.value=1
-test()
+servo = Servo(26,min_pulse_width=0.00055, max_pulse_width=0.00235)
+
