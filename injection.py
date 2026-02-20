988d0d5674f648b0650d10b189ad5e11f027af8b
Nick <170993098+Nick2425@users.noreply.github.com>
2026-02-20 14:29:45 -0500

    Adjusted constant notation

--- injection.py
+++ injection.py
@@ -3,9 +3,9 @@ import sys
 import time
 from gpiozero import Servo
 #Radius = #enter radius
-#Conver_Constant = math.pi * Radius * 2
-Conver_Constant = 6*math.pi
+#CONVER_CONSTANT = math.pi * Radius * 2
+CONVER_CONSTANT = 6*math.pi
 def inject_amount(count, dosage):
   current_amount = (count+1) * dosage
-  distance = ((current_amount / Conver_Constant)*(2/1.7))-1
+  distance = ((current_amount / CONVER_CONSTANT)*(2/1.7))-1
   return distance
--- testing modules/injection_test.py
+++ testing modules/injection_test.py
@@ -3,11 +3,11 @@ import sys
 import time
 from gpiozero import Servo
 Radius = 5
-Conver_Constant = math.pi * Radius * 2
+CONVER_CONSTANT = math.pi * Radius * 2
 servo = Servo(26,min_pulse_width=0.00055, max_pulse_width=0.00235)
 def inject_amount(count, dosage):
     current_amount = (count+1) * dosage
-    distance = round(((current_amount / Conver_Constant)*(2/1.7)),1)-1
+    distance = round(((current_amount / CONVER_CONSTANT)*(2/1.7)),1)-1
     return distance
 dosage = 2
 servo.min()
