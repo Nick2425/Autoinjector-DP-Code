8beae57dad1383f1ba5475d9200040fc45f5f18e
yudawenya20-svg <yudawenya20@gmail.com>
2026-02-19 22:31:49 -0500

    Update injection_test.py

--- testing modules/injection_test.py
+++ testing modules/injection_test.py
@@ -1,4 +1,15 @@
+import math
+import sys
+import time
+from gpiozero import Servo
+Radius = 5
+Conver_Constant = math.pi * Radius * 2
+servo = Servo(26,min_pulse_width=0.00055, max_pulse_width=0.00235)
 def inject_amount(count, dosage):
-  current_amount = (count+1) * dosage
-  distance = round(((current_amount / Conver_Constant)*(2/1.7)),1)-1
-  return distance
+    current_amount = (count+1) * dosage
+    distance = round(((current_amount / Conver_Constant)*(2/1.7)),1)-1
+    return distance
+dosage = 2
+for i in range(3):
+  servo.value(inject_amount(i,dosage))
+
