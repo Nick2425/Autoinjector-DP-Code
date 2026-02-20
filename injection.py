b99f3f5a49fbb494ccff46fd8837948dd0943497
yudawenya20-svg <yudawenya20@gmail.com>
2026-02-19 21:20:30 -0500

    Update injection.py

--- injection.py
+++ injection.py
@@ -5,4 +5,5 @@ from gpiozero import Servo
 #Radius = #enter radius
 #Conver_Constant = math.pi * Radius * 2
 servo = Servo(26,min_pulse_width=0.00055, max_pulse_width=0.00235)
-
+def inject_amount(count, dosage):
+  current_amount = count * dosage
