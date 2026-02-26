7729d9fc3a4f12d73df5291e7cdb3302e7e6f9fc
yudawenya20-svg <yudawenya20@gmail.com>
2026-02-26 17:22:49 -0500

    Update main_shell.py

--- main_shell.py
+++ main_shell.py
@@ -5,12 +5,12 @@ from sensor_library import *
 import injection
 
 ### Defines Global Constants
-LOOP_DELAY = 0.5
+DATA_POINT_DELAY = 0.5
 FORCE_THRESHOLD = 100
 HOLD_TIME = 3
 SYRINGE_VOLUME = 10 
 
-def main(dosage_period = 0):
+def main(time_between_doses = 0):
     green_led = create_objects.GREEN_LED
     red_led = create_objects.RED_LED
     buzzer = create_objects.BUZZER
@@ -20,112 +20,109 @@ def main(dosage_period = 0):
     servo.detach()
 
     ### Doctor inputs amount of medication per delivery & length of wait time in between deliveries
-    dosage_amount = 0
-    if dosage_period == 0:
+    medication_volume_per_delivery = 0
+    if time_between_doses == 0:
         try: 
-            dosage_period = float(input("Enter dosage period: "))    
+            time_between_doses = float(input("Enter dosage period: "))    
         except:
             print("Error: Enter a number for the dosage period!")
             main()
             return 0
     try:
-        dosage_amount = input("Enter the dosage amount in mL: ")
-        if str(int(dosage_amount)) != dosage_amount:
+        medication_volume_per_delivery = input("Enter the dosage amount in mL: ")
+        if str(int(medication_volume_per_delivery)) != medication_volume_per_delivery:
             print("Error: Enter an integer value for the dosage amount!")
-            main(dosage_period)
+            main(time_between_doses)
             return 0
     except:
         print("Error: Enter an integer value for the dosage amount!")
-        main(dosage_period)
+        main(time_between_doses)
         return 0
-    print("Success!")
-    dosage_amount = int(dosage_amount)
-    dosage_period = int(dosage_period)
-    FSR_list = [rv.FSR1, rv.FSR2, rv.FSR3, button_sensor]
-    doses_administered = 0
-    dosage_count = SYRINGE_VOLUME / dosage_amount
-    while doses_administered < dosage_count:
-        print_outputs()
+    
+    medication_volume_per_delivery = int(medication_volume_per_delivery)
+    time_between_doses = int(time_between_doses)
+    FSR_list = [rv.FSR1, rv.FSR2, rv.FSR3, button_sensor]  ## organizes the force sensing resistors *FSR*
+    doses_delivered = 0
+    total_delivery_count = SYRINGE_VOLUME / medication_volume_per_delivery
+    while doses_delivered < total_delivery_count:
+        print_outputs()  ## prints all outputs after each device status update
         red_led.off()
-        gate_open = False
+        is_sliding_cover_open = False
         green_led.on()
         buzzer.on()
         time.sleep(0.4)
         print_outputs(True)
         buzzer.off()
         print_outputs(True)
-        data_list = [[0],[0],[0]] 
-        RA_list = [0,0,0]               
-        above_threshold = False     
-        time_pressed = 0            
-        while gate_open == False:
-            print_outputs(True, RA_list, [data_list[0][-1], data_list[1][-1], data_list[2][-1], button_sensor.force_raw()])
+        raw_force_values = [[0],[0],[0]] 
+        rolling_average_list = [0,0,0]                   
+        contact_time = 0            
+        while is_sliding_cover_open == False:
+            print_outputs(True, rolling_average_list, [raw_force_values[0][-1], raw_force_values[1][-1], raw_force_values[2][-1], button_sensor.force_raw()])
             try:
                 if button_sensor.force_raw() > FORCE_THRESHOLD:
-                    gate_open = True
+                    is_sliding_cover_open = True
                     green_led.off()
                     dc_motor.forward(speed=gate.SPEED)
-                    print_outputs(True, RA_list, [data_list[0][-1], data_list[1][-1], data_list[2][-1], button_sensor.force_raw()])
+                    print_outputs(True, rolling_average_list, [raw_force_values[0][-1], raw_force_values[1][-1], raw_force_values[2][-1], button_sensor.force_raw()])
                     time.sleep(gate.TIME)
                     dc_motor.stop()
-                    print_outputs(True, RA_list, [data_list[0][-1], data_list[1][-1], data_list[2][-1], button_sensor.force_raw()])
-            except:
-                print("Error opening the gate")
-            time.sleep(LOOP_DELAY) 
-        while above_threshold != True:
-            i = 0
-            while i < 3:
+                    print_outputs(True, rolling_average_list, [raw_force_values[0][-1], raw_force_values[1][-1], raw_force_values[2][-1], button_sensor.force_raw()])
+            except Exception as e:   ### Accounts for potential errors in DC Motor movement including inability to rotate due to a blockage
+                print(f"An unexpected error has occurred: {e}")
+            time.sleep(DATA_POINT_DELAY)
+        ### code below compares rolling force averages to threshold force & tracks contact time between injector and dock
+        is_rolling_average_above_threshold = False
+        while is_rolling_average_above_threshold != True:
+            for i in range(3):
                 print(i)
-                data_list[i] = rv.update_list(data_list[i], FSR_list[i])
-                RA_list[i] = rv.FSR_rolling_average(data_list[i])
-                i+=1
-            print_outputs(True, RA_list, [data_list[0][-1], data_list[1][-1], data_list[2][-1], button_sensor.force_raw()], time_pressed)
-            if RA_list[0] != None and RA_list[1] != None and RA_list[2] != None:
-                if RA_list[0] > FORCE_THRESHOLD and RA_list[1] > FORCE_THRESHOLD and RA_list[2] > FORCE_THRESHOLD: 
-                    time_pressed += LOOP_DELAY
-                    if time_pressed > HOLD_TIME:                                                                   
-                        above_threshold = True 
+                raw_force_values[i] = rv.update_list(raw_force_values[i], FSR_list[i])
+                rolling_average_list[i] = rv.FSR_rolling_average(raw_force_values[i])
+            print_outputs(True, rolling_average_list, [raw_force_values[0][-1], raw_force_values[1][-1], raw_force_values[2][-1], button_sensor.force_raw()], contact_time)
+            if rolling_average_list[0] != None and rolling_average_list[1] != None and rolling_average_list[2] != None:
+                if rolling_average_list[0] > FORCE_THRESHOLD and rolling_average_list[1] > FORCE_THRESHOLD and rolling_average_list[2] > FORCE_THRESHOLD: 
+                    contact_time += DATA_POINT_DELAY
+                    if contact_time > HOLD_TIME:                                                                   
+                        is_rolling_average_above_threshold = True 
                         break                                                                                      
                 else:                           
-                    if time_pressed > 0:        
-                        time_pressed = 0        
-            time.sleep(LOOP_DELAY) 
-            rv.time_passed += LOOP_DELAY
+                    if contact_time > 0:        
+                        contact_time = 0        
+            time.sleep(DATA_POINT_DELAY) 
+            rv.time_passed += DATA_POINT_DELAY
 
-        ##### BEGIN INJECTION HERE
-
-        servo.value = inject_amount(doses_administered, int(dosage_amount))
-        print_outputs(True, RA_list, [data_list[0][-1], data_list[1][-1], data_list[2][-1], button_sensor.force_raw()], time_pressed)
+        ### code below begins delivery of medication
+        servo.value = inject_amount(doses_delivered, medication_volume_per_delivery)
+        print_outputs(True, rolling_average_list, [raw_force_values[0][-1], raw_force_values[1][-1], raw_force_values[2][-1], button_sensor.force_raw()], contact_time)
         time.sleep(1)
         servo.detach()  
-        print_outputs(True, RA_list, [data_list[0][-1], data_list[1][-1], data_list[2][-1], button_sensor.force_raw()], time_pressed)
-        force_list = []  
+        print_outputs(True, rolling_average_list, [raw_force_values[0][-1], raw_force_values[1][-1], raw_force_values[2][-1], button_sensor.force_raw()], contact_time)
+        ### code below checks whether the user maintained injector-dock contact for the entire delivery
+        check_raw_force_list = []  
         for i in range(3):
-            force_list.append(FSR_list[i].force_raw())
-        if force_list[0] > FORCE_THRESHOLD and force_list[1] > FORCE_THRESHOLD and force_list[2] > FORCE_THRESHOLD:
-            pass
-        else:
-            print('potential incompletion in delivery')
-        force_list_2 = []
-        for i in force_list:
-            force_list_2.append(i)
-        force_list_2.append(button_sensor.force_raw()) 
-        print_outputs(True, RA_list, force_list_2, time_pressed)
-        time.sleep(5)
+            check_raw_force_list.append(FSR_list[i].force_raw())
+        if not(check_raw_force_list[0] > FORCE_THRESHOLD and check_raw_force_list[1] > FORCE_THRESHOLD and check_raw_force_list[2] > FORCE_THRESHOLD):
+            print('Potential incompletion in delivery')
+        check_raw_force_list.append(button_sensor.force_raw()) 
+        print_outputs(True, rolling_average_list, check_raw_force_list, contact_time)
+        time.sleep(3)  ## gives user time to remove injector from the dock before the cover closes
         try:
             dc_motor.backward(speed=gate.SPEED)
-            print_outputs(True, RA_list, force_list_2, time_pressed)
+            print_outputs(True, rolling_average_list, check_raw_force_list_2, contact_time)
             time.sleep(gate.TIME)
             dc_motor.stop()
-            print_outputs(True, RA_list, force_list_2, time_pressed)
-        except:
-            print("Error closing the gate")
-        doses_administered += 1
+            print_outputs(True, rolling_average_list, check_raw_force_list_2, contact_time)
+        except Exception as e: ### Accounts for potential errors in DC Motor movement including inability to rotate when injector is still in contact with the dock
+            print(f"An unexpected error occured: {e}")
+        finally:
+            pass
+        doses_delivered += 1
         red_led.on()
-        print_outputs(True, RA_list, force_list_2, time_pressed)
-        time.sleep(dosage_period) 
-    servo.min()  
-    time.sleep(1)
+        print_outputs(True, rolling_average_list, check_raw_force_list_2, contact_time)
+        time.sleep(time_between_doses)  
+    ### code below resets the device once all meedication in the injector has been delivered
+    servo.min()  ## resets syringe push position 
+    time.sleep(1)  ## gives the servo motor time to process
     servo.detach()
 
 
@@ -138,7 +135,7 @@ def inject_amount(count, dosage):
   return distance
 
 ### PRINTS THE OUTPUTS OF THE PROGRAM IN AN ORDERED FASHION
-def print_outputs(pass_title = False, rolling_average_list = ["N/A","N/A","N/A"], force_raw_list = ["N/A","N/A","N/A","N/A"], time_pressed = 0):
+def print_outputs(pass_title = False, rolling_average_list = [None,None,None], force_raw_list = [None,None,None,None], contact_time = 0):
     if pass_title != True:
         titles = ["Red LED", 
                   "Green LED", 
@@ -167,7 +164,7 @@ def print_outputs(pass_title = False, rolling_average_list = ["N/A","N/A","N/A"]
             force_raw_list[2],
             rolling_average_list[2], 
             force_raw_list[3],
-            round(time_pressed,2),
+            round(contact_time,2),
             rotate_mapping(create_objects.SERVO.is_active),
             rotate_mapping(create_objects.DC_MOTOR.is_active)]
     string = ""
