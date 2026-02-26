a029906a22c60ff10e5ff4f9d63d7dfc1f88f48c
Nick <170993098+Nick2425@users.noreply.github.com>
2026-02-26 15:38:24 -0500

    Updated comments

--- main_shell.py
+++ main_shell.py
@@ -7,31 +7,25 @@ import injection
 LOOP_DELAY = 0.5
 FORCE_THRESHOLD = 100
 HOLD_TIME = 3
-SYRINGE_VOLUME = 10 ## PLACEHOLDER
-
+SYRINGE_VOLUME = 10 
 
 def main(dosage_period = 0):
-    #### THIS ASSIGNS OBJECTS TO VARIABLES
     green_led = create_objects.GREEN_LED
     red_led = create_objects.RED_LED
     buzzer = create_objects.BUZZER
     dc_motor = create_objects.DC_MOTOR
     servo = create_objects.SERVO
     button_sensor = create_objects.FSR
-
     servo.detach()
-
     dosage_amount = 0
-    ### Each return statement after main ensures that main() won't be called continuously.
-    ###### INPUT INFORMATION BY DOCTOR ########
     if dosage_period == 0:
-        try: ### CHECK TYPE OF INPUT
+        try: 
             dosage_period = float(input("Enter dosage period: "))    
         except:
             print("Error: Enter a number for the dosage period!")
             main()
             return 0
-    try: #### CHECK TYPE OF INPUT
+    try:
         dosage_amount = input("Enter the dosage amount in mL: ")
         if str(int(dosage_amount)) != dosage_amount:
             print("Error: Enter an integer value for the dosage amount!")
@@ -42,48 +36,27 @@ def main(dosage_period = 0):
         main(dosage_period)
         return 0
     print("Success!")
-
-
-
-    ### ESTABLISHES AMOUNT OF DOSES AND PERIOD
     dosage_amount = int(dosage_amount)
     dosage_period = int(dosage_period)
-
-    ############### TIME LIST DEFINITION AND FORCE SENSOR OBJECTS ################
     FSR_list = [rv.FSR1, rv.FSR2, rv.FSR3, button_sensor]
-
-    #### ESTABLISH INITIAL DOSAGE COUNT AND DOSES ADMINISTERED
     doses_administered = 0
     dosage_count = SYRINGE_VOLUME / dosage_amount
-
-
-    ###### MAIN LOOP BEGINS HERE #####################
     while doses_administered < dosage_count:
         print_outputs()
-        ######### initialize gate and LEDs
         red_led.off()
         gate_open = False
         green_led.on()
-        
-        ######### BUZZER BEEPS HERE
         buzzer.on()
         time.sleep(0.4)
         print_outputs(True)
         buzzer.off()
-        ###################33
         print_outputs(True)
-
-        data_list = [[0],[0],[0]]   #### EMPTY DATA SETS
-        RA_list = [0,0,0]                #### DEFINES / CLEARS THE ROLLING AVERAGE LIST
-
-        above_threshold = False     #### USED FOR ROLLING AVERAGE CHECKING
-        time_pressed = 0            #### USED TO DETECT IF 3 MAIN FORCE SENSORS ARE HELD FOR 3+ SECONDS
-
-        ########### While the gate isn't --- program halts until the button is pressed and gate opens
+        data_list = [[0],[0],[0]] 
+        RA_list = [0,0,0]               
+        above_threshold = False     
+        time_pressed = 0            
         while gate_open == False:
             print_outputs(True, RA_list, [data_list[0][-1], data_list[1][-1], data_list[2][-1], button_sensor.force_raw()])
-
-            ############# Check the force sensor
             try:
                 if button_sensor.force_raw() > FORCE_THRESHOLD:
                     gate_open = True
@@ -93,18 +66,10 @@ def main(dosage_period = 0):
                     time.sleep(gate.TIME)
                     dc_motor.stop()
                     print_outputs(True, RA_list, [data_list[0][-1], data_list[1][-1], data_list[2][-1], button_sensor.force_raw()])
-
             except:
                 print("Error opening the gate")
-
-            ######## MATPLOTLIB ########
             time.sleep(LOOP_DELAY) 
-
-        ###### Begin calculating the rolling average of FSRS
-        ## Incorporate rolling average code here.
-
         while above_threshold != True:
-            ##### RA IS THE SAME AS ROLLING AVERAGE
             i = 0
             while i < 3:
                 print(i)
@@ -112,29 +77,23 @@ def main(dosage_period = 0):
                 RA_list[i] = rv.FSR_rolling_average(data_list[i])
                 i+=1
             print_outputs(True, RA_list, [data_list[0][-1], data_list[1][-1], data_list[2][-1], button_sensor.force_raw()], time_pressed)
-            #### Compares rolling averages and checks if they are defined or are null
             if RA_list[0] != None and RA_list[1] != None and RA_list[2] != None:
-                if RA_list[0] > FORCE_THRESHOLD and RA_list[1] > FORCE_THRESHOLD and RA_list[2] > FORCE_THRESHOLD:  ### imcrements time the sensor is pushed for
+                if RA_list[0] > FORCE_THRESHOLD and RA_list[1] > FORCE_THRESHOLD and RA_list[2] > FORCE_THRESHOLD: 
                     time_pressed += LOOP_DELAY
-                                                                      ### adjusts for unknown time delay
-                    if time_pressed > HOLD_TIME:                                                                    ### checks if sensor is held for the total hold time
+                    if time_pressed > HOLD_TIME:                                                                   
                         above_threshold = True 
-                        break                                                                                       ### This exists the loop
-                else:                           #### checks if person lets go and resets if necessary.
-                    if time_pressed > 0:        #### Resets the hold time if the averages aren't all above 0
-                        time_pressed = 0        #### which can imply that the device is in contact at the wrong angle and therefore can malfunction
-
+                        break                                                                                      
+                else:                           
+                    if time_pressed > 0:        
+                        time_pressed = 0        
             time.sleep(LOOP_DELAY) 
             rv.time_passed += LOOP_DELAY
-
-        ##### BEGIN INJECTION HERE
-
         servo.value = inject_amount(doses_administered, int(dosage_amount))
         print_outputs(True, RA_list, [data_list[0][-1], data_list[1][-1], data_list[2][-1], button_sensor.force_raw()], time_pressed)
         time.sleep(1)
-        servo.detach()  ## to avoid jittering
+        servo.detach()  
         print_outputs(True, RA_list, [data_list[0][-1], data_list[1][-1], data_list[2][-1], button_sensor.force_raw()], time_pressed)
-        force_list = []  ## checks if user was still holding in proper position
+        force_list = []  
         for i in range(3):
             force_list.append(FSR_list[i].force_raw())
         if force_list[0] > FORCE_THRESHOLD and force_list[1] > FORCE_THRESHOLD and force_list[2] > FORCE_THRESHOLD:
@@ -146,11 +105,7 @@ def main(dosage_period = 0):
             force_list_2.append(i)
         force_list_2.append(button_sensor.force_raw()) 
         print_outputs(True, RA_list, force_list_2, time_pressed)
-
-        
-        ##### Grace period after injection
         time.sleep(5)
-        ##### Close the gate
         try:
             dc_motor.backward(speed=gate.SPEED)
             print_outputs(True, RA_list, force_list_2, time_pressed)
@@ -159,35 +114,42 @@ def main(dosage_period = 0):
             print_outputs(True, RA_list, force_list_2, time_pressed)
         except:
             print("Error closing the gate")
-
-
-        ### Finishing the dose administration
         doses_administered += 1
         red_led.on()
         print_outputs(True, RA_list, force_list_2, time_pressed)
-        time.sleep(dosage_period) ### Waits for dosage period.
-
-    #### End of autoinjector use - needs refill now.
-    servo.min()  ## resets linear actuator position
+        time.sleep(dosage_period) 
+    servo.min()  
     time.sleep(1)
     servo.detach()
 
 
 
 
-### CALCULATES SEVRO MOTOR PUSH DISTANCE
+### CALCULATES AND RETURNS THE POSITION THAT THE SERVO MOTOR MUST BE SET TO BASED ON CURRENT DOSAGE COUNT
 def inject_amount(count, dosage):
   current_amount = (count+1) * dosage
   distance = ((current_amount / injection.CONVER_CONSTANT)*(2/1.7))-1
   return distance
 
-    
+### PRINTS THE OUTPUTS OF THE PROGRAM IN AN ORDERED FASHION
 def print_outputs(pass_title = False, rolling_average_list = ["N/A","N/A","N/A"], force_raw_list = ["N/A","N/A","N/A","N/A"], time_pressed = 0):
     if pass_title != True:
-        titles = ["Red LED", "Green LED", "Buzzer", "FSR 1 (raw)", "FSR 1 (avg)", "FSR 2 (raw)", "FSR 2 (avg)", "FSR 3 (raw)", "FSR 3 (avg)", "FSR 4 (raw)", "Time Pressed", "Servo Motor", "DC Motor"]
+        titles = ["Red LED", 
+                  "Green LED", 
+                  "Buzzer", 
+                  "FSR 1 (raw)", 
+                  "FSR 1 (avg)", 
+                  "FSR 2 (raw)", 
+                  "FSR 2 (avg)", 
+                  "FSR 3 (raw)", 
+                  "FSR 3 (avg)", 
+                  "FSR 4 (raw)", 
+                  "Time Pressed", 
+                  "Servo Motor", 
+                  "DC Motor"]
         string = ""
         for i in titles:
-            string += f"{i:<12}"
+            string += f"{i:<13}"
         print(string)
     data = [on_off_mapping(create_objects.RED_LED.is_active), 
             on_off_mapping(create_objects.GREEN_LED.is_active), 
@@ -204,15 +166,18 @@ def print_outputs(pass_title = False, rolling_average_list = ["N/A","N/A","N/A"]
             rotate_mapping(create_objects.DC_MOTOR.is_active)]
     string = ""
     for i in data:
-        string += f"{str(i):<12}"
+        string += f"{str(i):<13}"
     print(string)
 
 
+### THIS CONVERTS TRUE OR FALSE STATEMENTS INTO ON OR OFF STRINGS FOR OUR LEDS
 def on_off_mapping(active):
     if active == True:
         return "On"
     else:
         return "Off"
+    
+### THIS CONVERTS TRUE OR FALSE STATEMENTS INTO ROTATING OR OFF STRINGS FOR OUR MOTORS
 def rotate_mapping(active):
     if active == True:
         return "Rotating"
